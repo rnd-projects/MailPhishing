e420c1eb440c1afb3bf523cd1df6982b2fce55d9
Ahmad Alhour <a.z.alhour@gmail.com>
2014-09-25 14:58:56 +0300

    Add a grid of 24x24 cells. Add a button, a searchbox, a lable and an empty data-grid to the window-grid.

--- Mail-Phishing/MainWindow.xaml.cs
+++ Mail-Phishing/MainWindow.xaml.cs
@@ -23,6 +23,44 @@ namespace Mail_Phishing
         public MainWindow()
         {
             InitializeComponent();
+
+            //DLGrid.Columns = 
+        }
+
+        private void SampleProgramCode()
+        {
+            //MailerUtil mailer = new MailerUtil();
+            //DistributionListUtil dlu = new DistributionListUtil();
+
+            ////create appropriate functions for DL and DDL
+            //GetMembersEmail DLEmails = new GetMembersEmail(dlu.listDLMembers);
+            //GetMembersEmail DDLEmails = new GetMembersEmail(dlu.listDDLMembers);
+
+            ////MailerUtil.GetDistributionListMembersDelegate dluHandler = new MailerUtil.GetDistributionListMembersDelegate(dlu.GetDistributionListMembers);
+
+
+
+            ////Calling It directly
+            //Func<string, List<string>> dluHandler = (param) => DLEmails(param);
+            //Func<string, List<string>> ddluHandler = (param) => DDLEmails(param);
+            ////List<string> xxx = ddluHandler.Invoke("(&(physicalDeliveryOfficeName=MOA)(!(name=SystemMailbox{*))(!(name=CAS_{*))(!(msExchRecipientTypeDetails=16777216))(!(msExchRecipientTypeDetails=536870912))(!(msExchRecipientTypeDetails=8388608)))");
+
+            ////dlu.GetDynamicDistributionLists();
+            ////dlu.GetDistributionLists();
+
+            ////calling it through Mailer
+            //mailer.SenMail(ddluHandler, new object[] { "(&(physicalDeliveryOfficeName=MOA)(!(name=SystemMailbox{*))(!(name=CAS_{*))(!(msExchRecipientTypeDetails=16777216))(!(msExchRecipientTypeDetails=536870912))(!(msExchRecipientTypeDetails=8388608)))" });
+        }
+
+        private void DLSearchSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = DLSearchTextbox.Text;
+
+        }
+
+        private void DLSearchTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
